c3c1600c2c5800465bbdc0277160cba4c780cf14
Alon Weissfeld <eldan.yonatan@gmail.com>
2019-01-12 18:47:10 +0200

    created buttons and added css code after fix

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,10 @@
-const Midi = Tone.Midi;
+//const Midi = Tone.Midi;
 const Synth = Tone.Synth;
+
+var synth = new Synth.toMaster();
 
 function playC () {
     console.log("Pressed.")
     // Play middle 'C'
-    synth.triggerAttackRelease(notes[1].toFrequency(), "8n");
+    synth.triggerAttackRelease("C4", "8n");
 }
