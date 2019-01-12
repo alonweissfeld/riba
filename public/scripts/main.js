7ee62f98ff393271e292781c0700f43c2cafec02
Alon Weissfeld <eldan.yonatan@gmail.com>
2019-01-12 20:54:23 +0200

    drag and drop finished

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,12 @@
 
-const Midi = Tone.Midi;
+//const Midi = Tone.Midi;
 const Synth = Tone.Synth;
 var synth = new Synth().toMaster();
-var notes = getMajorPentatonic(c4);
+var notes = getMinorPentatonic(c4);
 
-function playC () {
+function playnote (index) {
     console.log("Pressed.")
     // Play middle 'C'
-
-    synth.triggerAttackRelease(notes[3], "8n");
+    if(index>= 7){ synth.triggerAttackRelease(notes[index%7]*2, "8n");}
+    else{ synth.triggerAttackRelease(notes[index], "8n"); }
 }
