b0349a8cc14df312be02dc1ffb5688d17f107973
Alon Weissfeld <alonweissfeld@gmail.com>
2019-03-10 10:27:26 +0200

    Listen to process.ENV

--- app.js
+++ app.js
@@ -23,4 +23,4 @@ app.get('/play', function(req, res) {
     });
 })
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log("Listening on port 3000"));
